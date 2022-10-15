611d5578316447dca6c6fb74aa947d3d697a9131
lmh <dev@hilberg.eu>
2022-10-15 17:27:06 +0200

    start daemon

--- setup.sh
+++ setup.sh
@@ -3,6 +3,20 @@
 DEB="https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
 DIR="/tmp/tv"
 
+# start teamviewer daemon and optionally set PW
+start_teamviewer_daemon() {
+  # cleanup running teamviewer
+  sudo killall /opt/teamviewer/tv_bin/TeamViewer >/dev/null 2>&1 || :
+  # ensure systemctl symlink and start teamviewer daemon
+  sudo teamviewer daemon enable >/dev/null 2>&1
+  sudo systemctl start teamviewerd.service >/dev/null 2>&1
+  sudo teamviewer --daemon start >/dev/null 2>&1
+  # wait a bit until teamviewer daemon is started
+  sleep 5
+  # ensure license is accepted
+  sudo teamviewer license accept >/dev/null 2>&1
+}
+
 mkdir -p "$DIR"
 cd "$DIR"
 
@@ -10,4 +24,4 @@ wget -P "$DIR" "$DEB"
 
 sudo dpkg --install "$DIR/teamviewer_amd64.deb"
 
-/usr/bin/teamviewer
+start_teamviewer_daemon
